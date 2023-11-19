0e2a34049fd4045d14a70a452850181c37f1af01
Nuno Colaço <nunocolaco@Nunos-MacBook-Pro.local>
2023-11-19 18:25:24 +0000

    Start the navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="App" id="outer-container">
+      <Navbar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      <div id="page-wrap">
+      </div>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
+import NavbarCross from "../../Assets/NavbarAssets/NavbarCross.png";
+import BurgerMenu from "../../Assets/NavbarAssets/HamburguerMenu.png";
+import "./Navbar.css";
 
-const Navbar = () => {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
   return (
-    <div>Navbar</div>
-  )
-}
-
-export default Navbar
+    <Menu
+      right
+      customBurgerIcon={<img src={BurgerMenu} alt="burger-menu" />}
+      customCrossIcon={<img src={NavbarCross} alt="cross" />}
+    >
+      <a id="home" className="menu-item" href="/">
+        Início
+      </a>
+      <a id="about" className="menu-item" href="/about">
+        Metodologias
+      </a>
+      <a id="contact" className="menu-item" href="/contact">
+        Soluções
+      </a>
+      <a id="settings" className="menu-item--small" href="">
+        Agendar
+      </a>
+    </Menu>
+  );
+};
