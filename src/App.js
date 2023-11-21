1b0fe2b17cabb5de726cb75181eefc7c921e4879
Nuno Colaço <nunocolaco@Nunos-MacBook-Pro.local>
2023-11-21 21:50:55 +0000

    Add the routing across the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./components/HomePage/HomePage.jsx";
+import MethodologiesPage from "./components/MethodologiesPage/MethodologiesPage.jsx";
+import SolutionsPage from "./components/SolutionsPage/SolutionsPage.jsx";
+import WhoWeArePage from "./components/WhoWeArePage/WhoWeArePage.jsx";
+import FormPage from "./components/FormPage/FormPage.jsx";
 
 function App() {
   return (
     <div className="App" id="outer-container">
-      <Navbar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-      <div id="page-wrap">
-      </div>
+      <Router>
+        <Navbar pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
+        <div id="page-wrap"></div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/metodologias" element={<MethodologiesPage />} />
+          <Route path="/quem-somos" element={<WhoWeArePage />} />
+          <Route path="/solucoes" element={<SolutionsPage />} />
+          <Route path="/agendar" element={<FormPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,46 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
+// import { Link } from "react-router-dom";
 import NavbarCross from "../../Assets/NavbarAssets/NavbarCross.png";
 import BurgerMenu from "../../Assets/NavbarAssets/HamburguerMenu.png";
+import Logo from "../../Assets/LogoAssets/MainLogo.png";
 import "./Navbar.css";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  // const history = useHistory();
+
+  // const closeMenu = (path) => {
+  //   history.push(path);
+  // };
+
   return (
-    <Menu
-      right
-      customBurgerIcon={<img src={BurgerMenu} alt="burger-menu" />}
-      customCrossIcon={<img src={NavbarCross} alt="cross" />}
-    >
-      <a id="home" className="menu-item" href="/">
-        Início
-      </a>
-      <a id="about" className="menu-item" href="/about">
-        Metodologias
-      </a>
-      <a id="contact" className="menu-item" href="/contact">
-        Soluções
-      </a>
-      <a id="settings" className="menu-item--small" href="">
-        Agendar
-      </a>
-    </Menu>
+    <div className="navbar-menu-logo-container">
+      <div>
+        <img className="logo" src={Logo} alt="logo" />
+      </div>
+      <Menu
+        right
+        customBurgerIcon={<img src={BurgerMenu} alt="burger-menu" />}
+        customCrossIcon={<img src={NavbarCross} alt="cross" />}
+      >
+        <a href="/" id="home" className="menu-item">
+          Início
+        </a>
+        <a href="/metodologias" id="about" className="menu-item">
+          Metodologias
+        </a>
+        <a href="/quem-somos" id="about" className="menu-item">
+          Quem somos
+        </a>
+        <a href="/solucoes" id="contact" className="menu-item">
+          Soluções
+        </a>
+        <a href="/agendar" id="settings" className="menu-item">
+          Agendar
+        </a>
+      </Menu>
+    </div>
   );
 };
