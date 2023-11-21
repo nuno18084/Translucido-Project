6de0713ef6fadc833add311f1910203b316b68b2
Nuno Colaço <nunocolaco@Nunos-MacBook-Pro.local>
2023-11-21 23:53:33 +0000

    Start to develop the homepage. Start the image roller feature

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import "../../App.css";
+import "./HomePage.css";
+import ImageSlider from "./ImageSlider/ImageSlider";
+import Image1 from "../../Assets/HomePage/LandingAssets/LandingPageImages/Soft skill passível de ser desenvolvida.png";
+import Image2 from "../../Assets/HomePage/LandingAssets/LandingPageImages/diferente de simpatia.png";
+import Image3 from "../../Assets/HomePage/LandingAssets/LandingPageImages/perguntar se esta tudo bem e t er tempo para ouvir um não.png";
+import Image4 from "../../Assets/HomePage/LandingAssets/LandingPageImages/permitir trabalho híbrido.jpg";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const images = [Image1, Image2, Image3, Image4];
 
-export default HomePage
+  return (
+    <div>
+      <div className="image-slider-container">
+        <ImageSlider images={images} />
+      </div>
+      <div>
+        <h1>Empatia é</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/HomePage/ImageSlider/ImageSlider.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useState, useEffect } from "react";
+import "./ImageSlider.css";
+
+const ImageSlider = ({ images, transitionDuration = 4000 }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Change the current image index to the next one
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, transitionDuration);
+
+    return () => clearInterval(interval);
+  }, [currentImageIndex, images.length, transitionDuration]);
+
+  return (
+    <div className="image-slider">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Image ${index}`}
+          className={`slider-image ${
+            index === currentImageIndex ? "active" : ""
+          }`}
+          style={{
+            transition: `opacity ${transitionDuration / 2}ms ease-in-out`,
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ImageSlider;
