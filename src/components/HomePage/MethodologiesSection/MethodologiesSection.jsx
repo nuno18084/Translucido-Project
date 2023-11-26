a96cf9e95f9fb666062e8fadae5faebafa0829dc
Nuno Colaço <nunocolaco@Nunos-MBP.home>
2023-11-26 02:22:20 +0000

    Start the slider feature for the methodologies section

--- src/components/HomePage/MethodologiesSection/MethodologiesSection.jsx
+++ src/components/HomePage/MethodologiesSection/MethodologiesSection.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./MethodologiesSection.css";
+import teste from "../../../Assets/HomePage/MethodologiesAssets/MethodologiesCartoons/DesignThinking.png";
 
-const MethodologiesSection = () => {
-  return <div>MethodologiesSection</div>;
-};
+// import required modules
+import { Pagination, Navigation } from "swiper/modules";
 
-export default MethodologiesSection;
+export default function App() {
+  const [swiperRef, setSwiperRef] = useState(null);
+
+  let appendNumber = 4;
+  let prependNumber = 1;
+
+  const prepend2 = () => {
+    swiperRef.prependSlide([
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+    ]);
+  };
+
+  const prepend = () => {
+    swiperRef.prependSlide(
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+    );
+  };
+
+  const append = () => {
+    swiperRef.appendSlide(
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+    );
+  };
+
+  const append2 = () => {
+    swiperRef.appendSlide([
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+    ]);
+  };
+
+  return (
+    <>
+      <div className="methodologies-main-section">
+        <h1>Metedologias</h1>
+      </div>
+      <Swiper
+        onSwiper={setSwiperRef}
+        slidesPerView={1.5}
+        centeredSlides={true}
+        spaceBetween={10}
+        pagination={{
+          type: "fraction",
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={teste} alt="test" />
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
