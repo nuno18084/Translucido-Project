7b259962cba5b09009c40003622a9a35c41d6a26
Nuno Colaço <nunocolaco@Nunos-MacBook-Pro.local>
2023-11-29 20:29:29 +0000

    Solve a few issues on the homepage

--- src/components/HomePage/MethodologiesSection/MethodologiesSection.jsx
+++ src/components/HomePage/MethodologiesSection/MethodologiesSection.jsx
@@ -1,46 +1,71 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./MethodologiesSection.css";
-import teste from "../../../Assets/HomePage/MethodologiesAssets/MethodologiesCartoons/DesignThinking.png";
+import Arrow from "../../../Assets/HomePage/SolutionsAssets/SolutionsArrow.png";
+import Imersao from "../../../Assets/HomePage/MethodologiesAssets/MethodologiesCartoons/Imersão.png";
+import Sociodrama from "../../../Assets/HomePage/MethodologiesAssets/MethodologiesCartoons/Sociodrama.png";
+import Story from "../../../Assets/HomePage/MethodologiesAssets/MethodologiesCartoons/StoryTelling.png";
+import Design from "../../../Assets/HomePage/MethodologiesAssets/MethodologiesCartoons/DesignThinking.png";
 
 // import required modules
 import { Pagination, Navigation } from "swiper/modules";
 
-export default function App() {
+const CustomPagination = ({ swiper }) => {
+  useEffect(() => {
+    if (!swiper || !swiper.slides) {
+      return;
+    }
+
+    const updateActiveDot = () => {
+      const activeDot = document.querySelector(".custom-pagination-dot.active");
+      if (activeDot) {
+        activeDot.classList.remove("active");
+      }
+      const newActiveDot = document.querySelector(
+        `.custom-pagination-dot[data-index="${swiper.activeIndex}"]`
+      );
+      if (newActiveDot) {
+        newActiveDot.classList.add("active");
+      }
+    };
+
+    swiper.on("slideChange", updateActiveDot);
+
+    return () => {
+      swiper.off("slideChange", updateActiveDot);
+    };
+  }, [swiper]);
+
+  if (!swiper || !swiper.slides) {
+    return null;
+  }
+
+  const slides = Array.from({ length: swiper.slides.length });
+
+  const handleDotClick = (index) => {
+    swiper.slideTo(index);
+  };
+
+  return (
+    <div className="custom-pagination">
+      {slides.map((_, index) => (
+        <div
+          key={index}
+          className={`custom-pagination-dot`}
+          data-index={index}
+          onClick={() => handleDotClick(index)}
+        ></div>
+      ))}
+    </div>
+  );
+};
+
+export default function MethodologiesSection() {
   const [swiperRef, setSwiperRef] = useState(null);
-
-  let appendNumber = 4;
-  let prependNumber = 1;
-
-  const prepend2 = () => {
-    swiperRef.prependSlide([
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-    ]);
-  };
-
-  const prepend = () => {
-    swiperRef.prependSlide(
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
-    );
-  };
-
-  const append = () => {
-    swiperRef.appendSlide(
-      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
-    );
-  };
-
-  const append2 = () => {
-    swiperRef.appendSlide([
-      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-    ]);
-  };
 
   return (
     <div className="methodologies-container">
@@ -53,19 +78,89 @@ export default function App() {
         centeredSlides={true}
         spaceBetween={10}
         pagination={{
-          type: "fraction",
+          el: ".custom-pagination",
+          clickable: true,
+          renderBullet: function (index, className) {
+            return `<span class="${className} custom-pagination-dot" data-index="${index}"></span>`;
+          },
         }}
         navigation={true}
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide>
-          <img src={teste} alt="test" />
+          <div className="methodologies-cards">
+            <div>
+              <h1>Imersão</h1>
+              <p>
+                Criar ambientes ilusórios para estimular os sentidos, aprofundar
+                debates e despertar empatia ao compreender processos
+                neurológicos subjacentes.
+              </p>
+              <div className="methodologies-arrow">
+                <img src={Arrow} alt="arrow" />
+              </div>
+            </div>
+            <div className="methodologies-cartoons">
+              <img src={Imersao} alt="test" />
+            </div>
+          </div>
         </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>
+          <div className="methodologies-cards">
+            <div>
+              <h1>Sociodrama</h1>
+              <p>
+                Utilizar dramatização para conscientizar grupos sobre questões,
+                promovendo reflexão empática e explorando soluções sensíveis.
+              </p>
+              <div className="methodologies-arrow">
+                <img src={Arrow} alt="arrow" />
+              </div>
+            </div>
+            <div className="methodologies-cartoons">
+              <img src={Sociodrama} alt="test" />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="methodologies-cards">
+            <div>
+              <h1>Story telling</h1>
+              <p>
+                Criar ambientes ilusórios para estimular os sentidos, aprofundar
+                debates e despertar empatia ao compreender processos
+                neurológicos subjacentes.
+              </p>
+              <div className="methodologies-arrow">
+                <img src={Arrow} alt="arrow" />
+              </div>
+            </div>
+            <div className="methodologies-cartoons">
+              <img src={Story} alt="test" />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="methodologies-cards">
+            <div>
+              <h1>Design thinking</h1>
+              <p>
+                Criar ambientes ilusórios para estimular os sentidos, aprofundar
+                debates e despertar empatia ao compreender processos
+                neurológicos subjacentes.
+              </p>
+              <div className="methodologies-arrow">
+                <img src={Arrow} alt="arrow" />
+              </div>
+            </div>
+            <div className="methodologies-cartoons">
+              <img src={Design} alt="test" />
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
+      <CustomPagination swiper={swiperRef} />
     </div>
   );
 }
