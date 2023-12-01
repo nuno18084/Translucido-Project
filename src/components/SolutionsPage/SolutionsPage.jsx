93a726aa9fbec38431c989953db1343149a30b55
Nuno Colaço <nunocolaco@Nunos-MacBook-Pro.local>
2023-12-01 23:28:01 +0000

    Add the first element of the solutions page

--- src/components/SolutionsPage/SolutionsPage.jsx
+++ src/components/SolutionsPage/SolutionsPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./SolutionsPage.css";
+import eclipse from "../../Assets/SolutionsPage/SolutionsEclipse.png";
+import SolutionsCartoon from "../../Assets/SolutionsPage/SolutionsCartoon.png";
 
 const SolutionsPage = () => {
   return (
-    <div>SolutionsPage</div>
-  )
-}
+    <div>
+      <div className="solutions-main-container">
+        <div className="solutions-first-container">
+          <img src={eclipse} alt="eclipse" className="solutions-eclipse" />
+        </div>
+        <div className="solutions-center-container">
+          <div>
+            <h1 className="solutions-h1">Soluções</h1>
+          </div>
+          <div>
+            <img
+              src={SolutionsCartoon}
+              alt="solutions-cartoon"
+              className="solutions-cartoon"
+            />
+          </div>
+          <div>
+            <button className="solutions-btn">Ver Soluções</button>
+          </div>
+        </div>
+        <div>
+          <img src={eclipse} alt="eclipse" className="solutions-eclipse" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SolutionsPage
+export default SolutionsPage;
