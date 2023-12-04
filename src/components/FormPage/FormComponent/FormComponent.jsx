c478b4738c2eb3b6e5d0ab123bca9387f5b6e81f
Nuno Colaço <nunocolaco@Nunos-MacBook-Pro.local>
2023-12-04 22:57:19 +0000

    Finish the form page

--- src/components/FormPage/FormComponent/FormComponent.jsx
+++ src/components/FormPage/FormComponent/FormComponent.jsx
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 const notify = () =>
   toast.success("Sucesso!", {
     position: "top-right",
-    autoClose: 5000,
+    autoClose: 3000,
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
